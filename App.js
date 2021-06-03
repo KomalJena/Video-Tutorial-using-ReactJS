c72fe022d72c8513f3d3b8b8bb786661e095000f
Komal Jena <48814261+KomalJena@users.noreply.github.com>
2021-06-03 11:34:51 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import MainRoute from './videoTutorialsite/MainRoute'
+
+function App() {
+  return (
+    <div className="App">
+      <MainRoute />
+    </div>
+  );
+}
+
+export default App;
